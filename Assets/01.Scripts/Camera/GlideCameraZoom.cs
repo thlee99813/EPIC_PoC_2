a1d998556654d6f1ce41e677af58f5a5436f8b04
thlee99813@gmail.com <thlee99813@gmail.com>
2026-05-14 09:50:42 +0900

    [Add] 1차 POC 완성

--- Assets/01.Scripts/Camera/GlideCameraZoom.cs
+++ Assets/01.Scripts/Camera/GlideCameraZoom.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class GlideCameraZoom : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera _cinemachineCamera;
-    [SerializeField] private CinemachineFollow _cinemachineFollow;
+    [SerializeField] private CinemachinePositionComposer _positionComposer;
     [SerializeField] private PlayerGlide _playerGlide;
     [SerializeField] private float _defaultOrthographicSize = 4f;
     [SerializeField] private float _glideOrthographicSize = 5f;
@@ -13,26 +13,23 @@ public class GlideCameraZoom : MonoBehaviour
 
 
     private void LateUpdate()
-{
-    float targetSize = _playerGlide.IsGliding ? _glideOrthographicSize : _defaultOrthographicSize;
-    float sizeRatio = targetSize / _defaultOrthographicSize;
+    {
+        float targetSize = _playerGlide.IsGliding ? _glideOrthographicSize : _defaultOrthographicSize;
+        float sizeRatio = targetSize / _defaultOrthographicSize;
 
-    Vector3 targetOffset = new Vector3(
-        _defaultFollowOffset.x * sizeRatio,
-        _defaultFollowOffset.y * sizeRatio,
-        _defaultFollowOffset.z
-    );
-  /*  Vector3 targetOffset = new Vector3(
-            _defaultFollowOffset.x,
-            _defaultFollowOffset.y,
+        Vector3 targetOffset = new Vector3(
+            _defaultFollowOffset.x * sizeRatio,
+            _defaultFollowOffset.y * sizeRatio,
             _defaultFollowOffset.z
         );
-*/
-    LensSettings lens = _cinemachineCamera.Lens;
-    lens.OrthographicSize = Mathf.Lerp(lens.OrthographicSize, targetSize, _zoomSpeed * Time.deltaTime);
-    _cinemachineCamera.Lens = lens;
 
-    _cinemachineFollow.FollowOffset = Vector3.Lerp(_cinemachineFollow.FollowOffset, targetOffset, _zoomSpeed * Time.deltaTime);
-}
+        LensSettings lens = _cinemachineCamera.Lens;
+        lens.OrthographicSize = Mathf.Lerp(lens.OrthographicSize, targetSize, _zoomSpeed * Time.deltaTime);
+        _cinemachineCamera.Lens = lens;
+
+        _positionComposer.TargetOffset = Vector3.Lerp( _positionComposer.TargetOffset, new Vector3(targetOffset.x, targetOffset.y, 0f), _zoomSpeed * Time.deltaTime);
+
+        _positionComposer.CameraDistance = Mathf.Abs(_defaultFollowOffset.z);
+    }
 
 }
